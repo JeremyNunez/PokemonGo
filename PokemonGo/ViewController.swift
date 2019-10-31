d4a0173837e0a8a9f2c9a294af3951d14c3ab805
JeremyNunez <jeremi.nunez@tecsup.edu.pe>
2019-10-31 11:13:52 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Jeremi Nunez on 10/31/19.
+//  Copyright © 2019 Jeremi Nunez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
